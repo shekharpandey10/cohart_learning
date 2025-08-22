c0cad565ef64c0d90c9e0d2084a9d8a37e428803
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-08-22 14:29:15 +0530

    pdf scraping using puppeteer

---
+++ jsAdv/React/puppeteer-tutorial/webScraping.js
@@ -0,0 +1,18 @@
+import puppeteer from "puppeteer";
+
+(async()=>{
+    try{
+        const browser=await puppeteer.launch({headless:false})
+        const page=await browser.newPage()
+        await page.goto('https://yahoo.com')
+        const title=await page.title()
+        console.log(title)
+        const heading=await page.$eval('h1',(element)=>element.textContent)
+        console.log(heading)
+        await page.screenshot({path:'episode3.png'})
+        await page.pdf({path:'example.pdf',format:'A4'})
+        await browser.close()
+    }catch(err){
+        console.error(err)
+    }
+})()
