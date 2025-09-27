bb94ddee03cacafc7ff55054dc858f3c9d320220
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-09-27 14:48:56 +0530

    chat app braodcasting logic added

--- jsAdv/WebSocket/chatApp/src/index.ts
+++ jsAdv/WebSocket/chatApp/src/index.ts
@@ -1,18 +1,59 @@
-import {WebSocketServer,WebSocket} from 'ws'
-const wss=new WebSocketServer({port:8001})
-let userCount=0
-let allSocket:WebSocket[]=[]
-wss.on('connection',(socket)=>{
-    allSocket.push(socket)
-    userCount++ 
-    console.log('user connnected #',userCount)
+import { WebSocketServer, WebSocket } from 'ws'
+const wss = new WebSocketServer({ port: 8001 })
 
-    socket.on('message',(event)=>{
-        console.log('message recived ', event.toString()+ ' from user: ',userCount)
-      allSocket.forEach((soc,i)=>{
-        soc.send(`message from the user ${i+1} is : ${event.toString()}`)
-      })
-       
-    })
+let userCount = 0
+let allSocket: WebSocket[] = []
+
+const allUser: Record<string, WebSocket[]> = {}
+wss.on('connection', (socket) => {
+  allSocket.push(socket)
+  userCount++
+
+  socket.on('message', (message: string) => {
+    const data = JSON.parse(message.toString())
+    if (data.type === 'createRoom') {
+      if (!(data.payload.roomId in allUser)) {
+        allUser[data.payload.roomId] = [socket]
+        console.log( 'room created')
+      } else {
+        console.log('room is present')
+      }
+    }
+
+    if (data.type === 'join') {
+      const roomId = data.payload.roomId
+      if (data.payload.roomId in allUser) {
+        const user = allUser[roomId]?.find((x) => x === socket)
+        console.log('this is the user')
+        if (user) {
+          console.log('user is already present')
+        } else {
+          console.log(allUser, 'sifjaljflka')
+          allUser[roomId]?.push(socket)
+          console.log('user added')
+        }
+      }else{
+        console.log('room does not exists')
+      }
+    }
+
+    if (data.type === 'message') {
+      // console.log('jflkajfjakjfsjflkajlkfd')
+      if (data.payload.roomId in allUser) {
+        const room = allUser[data.payload.roomId]
+        if (room?.length) {
+          console.log(data.payload.message)
+          for (let i = 0; i < room.length; i++) {
+            room[i]?.send(data.payload.message)
+          }
+        }
+      }
+    }
+
+    //  console.log(data)
+  })
+
+  socket.on('disconnect', () => {
+    allSocket = allSocket.filter((x) => x != socket)
+  })
 })
-
