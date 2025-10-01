0b9f07451b6aa9ed7596b687235e8785d07190e3
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-10-01 12:10:08 +0530

    .

--- jsAdv/PGSQL/src/index.ts
+++ jsAdv/PGSQL/src/index.ts
@@ -7,6 +7,8 @@ const pgClient = new Client(
 // console.log(pgClient)
 const app = express()
 app.use(express.json())
+const INSERT_QUERY_USER = `INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING id;`
+const INSERT_QUERY_ADDRESS=`INSERT INTO addresses (city,country,pincode,user_id) VALUES($1,$2,$3,$4);`
 
 const connectDb = async () => {
   await pgClient.connect()
@@ -14,17 +16,38 @@ const connectDb = async () => {
   //   const result = await pgClient.query('select * from users;')
   //   console.log(result.rows)
 }
-connectDb()
+ await connectDb()
 
 app.post('/signup', async (req, res) => {
-  const { username, password, email } = req.body
+  const { username, password, email, city, country, pincode } = req.body
   console.log(username, password, email)
-  const response = await pgClient.query(
-    `INSERT INTO users (username,email,password) VALUES ('${username}','${email}','${password}')`
-  )
-
+  await pgClient.query("BEGIN;")
+  const response = await pgClient.query(INSERT_QUERY_USER, [
+    username,
+    email,
+    password,
+  ])
+  console.log(response.rows[0].id)
+  const id=response.rows[0].id
+  console.log('start..........')
+  const addressResponse=await pgClient.query(INSERT_QUERY_ADDRESS,[city,country,pincode,id])
+  console.log('end........')
+  await pgClient.query("COMMIT;")
+  console.log(addressResponse)
   res.json({
     msg: 'You have signed up',
+  })
+})
+
+app.get('/metadata',async(req,res)=>{
+  const id=req.query.id
+  const query=`SELECT users.id, users.username,users.email,addresses.city,addresses.country,addresses.pincode
+  FROM users JOIN addresses ON users.id=addresses.user_id
+  WHERE users.id=$1`
+  const response=await pgClient.query(query,[id])
+  console.log(response.rows)
+  res.json({
+   response: response.rows
   })
 })
 
